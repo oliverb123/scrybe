f45d34df6d88d23c71641070cab392e2af48d557
Oliver <oliverbrowne627@gmail.com>
2017-08-31 03:03:14 +0100

    Improved setup.py

---
+++ scrybe.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python
+import userLib
+userLib.Session().start()
--- setup.py
+++ setup.py
@@ -1,9 +1,29 @@
+#!/usr/bin/env python
 import sqlite3 as sql
+import os
 
-conn = sql.connect("scrybe.db")
+try:
+    os.remove("scrybe.db")
+except:
+    pass
+
+cwd = os.getcwd() + "/"
+
+conn = sql.connect(cwd + "scrybe.db")
 c = conn.cursor()
 command = "CREATE TABLE notes (id INTEGER PRIMARY KEY, title TEXT NOT NULL, body TEXT NOT NULL, createTime REAL NOT NULL, archived INTEGER NOT NULL, tags TEXT NOT NULL)"
 c.execute(command)
 conn.commit()
 conn.close()
+
+with open("~/.bashrc", "a+") as bashrc:
+    bashrc.write("alias scrybe='python " + cwd + "scrybe.py" + " '\n")
+
+os.system("source ~/.bashrc")
+
+with open(cwd + "dbLib.py", "r") as oldDbLib:
+    newDbLibString = oldDbLib.read().replace("scrybe.db", cwd + "scrybe.db")
+
+with open(cwd + "dbLib.py", "w") as newDbLib:
+    newDbLib.write(newDbLibString)
 
